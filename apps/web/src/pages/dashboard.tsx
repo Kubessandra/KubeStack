57cb2fc03786da14e84d06ad5e71ddeb6b68e2d3
Kubessandra <aymeric.beudaert@gmail.com>
2022-12-31 14:05:28 +0100

    chore(naming): change some namings and ui
    .

--- apps/web/src/pages/dashboard.tsx
+++ apps/web/src/pages/dashboard.tsx
@@ -1,25 +1,17 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { ReactElement } from "react";
 import Layout from "~/components/Layout";
-import Pricing from "~/components/Pricing";
 import { useSession } from "~/hooks/useSession";
 import auth from "~/utils/auth";
-import { trpc } from "~/utils/trpc";
 
 const Dashboard = () => {
-  const router = useRouter();
-  const customerPortalMutation =
-    trpc.payment.createCustomerPortal.useMutation();
   const session = useSession(true);
   if (!session) return null;
 
   return (
     <div>
-      <h1 className="text-2xl font-bold my-8">Welcome to videos</h1>
+      <h1 className="text-2xl font-bold my-8">Welcome to the dashboard</h1>
       <h2 className="my-4">{session.email}</h2>
-
-      <Pricing />
       <div className="flex flex-col items-center space-y-4 m-8">
         <Link
           className="inline-flex items-center rounded-md border border-transparent bg-gray-600 px-4 py-2 text-base font-medium text-white hover:bg-gray-700"
@@ -27,15 +19,6 @@ const Dashboard = () => {
         >
           Logout
         </Link>
-        <button
-          className="inline-flex items-center rounded-md border border-transparent bg-gray-600 px-4 py-2 text-base font-medium text-white hover:bg-gray-700"
-          onClick={async () => {
-            const url = await customerPortalMutation.mutateAsync();
-            router.push(url);
-          }}
-        >
-          Customer Portal
-        </button>
       </div>
     </div>
   );
--- apps/web/src/pages/plan.tsx
+++ apps/web/src/pages/plan.tsx
@@ -1,9 +1,31 @@
+import { useRouter } from "next/router";
 import { ReactElement } from "react";
 import Layout from "~/components/Layout";
 import Pricing from "~/components/Pricing";
+import { useSession } from "~/hooks/useSession";
+import { trpc } from "~/utils/trpc";
 
 const PlanPage = () => {
-  return <Pricing />;
+  const router = useRouter();
+  const session = useSession(true);
+  const customerPortalMutation =
+    trpc.payment.createCustomerPortal.useMutation();
+
+  if (!session) return null;
+  return (
+    <div className="flex-col items-center flex">
+      <Pricing />
+      <button
+        className="inline-flex items-center rounded-md border border-transparent bg-gray-600 px-4 py-2 text-base font-medium text-white hover:bg-gray-700"
+        onClick={async () => {
+          const url = await customerPortalMutation.mutateAsync();
+          router.push(url);
+        }}
+      >
+        Customer Portal
+      </button>
+    </div>
+  );
 };
 
 PlanPage.getLayout = (page: ReactElement) => <Layout>{page}</Layout>;
