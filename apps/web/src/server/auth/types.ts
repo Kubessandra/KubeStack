e139cde31cfbb5dddca30205231221483be1bd7a
Kubessandra <aymeric.beudaert@gmail.com>
2023-02-08 00:54:10 +0100

    fix: deadcode
    .

--- apps/web/src/server/auth/types.ts
+++ apps/web/src/server/auth/types.ts
@@ -2,8 +2,3 @@ export interface Session {
   externalId: string;
   email: string;
 }
-
-export type GetSessionFunc = (params: {
-  authorization: string;
-  cookies: Record<string, string | undefined>;
-}) => Promise<Session | null>;
